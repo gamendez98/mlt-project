9acf1c297164ed957326a03f1320a31d59debed5
gustavo <gamendez98@gmail.com>
2024-05-14 18:07:17 -0500

    dummy api

--- application/app.py
+++ application/app.py
@@ -1,8 +1,11 @@
 from typing import Union
 
+import spacy
 from fastapi import FastAPI
 
 app = FastAPI()
+
+nlp = spacy.load("es_dep_news_trf")
 
 
 @app.get("/")
@@ -10,6 +13,15 @@ def ok():
     return {"status": "OK"}
 
 
-@app.get("/items/{item_id}")
-def get_corrections(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/correct-sentence")
+def get_corrections(sentence: str):
+    # TODO: add actual correction logic
+    return {"sentence": "Esta es la oracion despues de ser corregida"}
+
+
+@app.post("/error-corrections")
+def get_corrections(sentence: str):
+    # TODO: add actual correction logic
+    doc = nlp(sentence)
+    dummy_corrections = ['$APPEND_palabra', '$DELETE', '$SPLIT_2', '$REPLACE_palabra'] + (['$KEEP'] * 100)
+    return {"words": ['@@PADDING@@'] + [token.text for token in doc], 'labels': dummy_corrections[:len(doc)]}
